970b3e4600fc6cd512feccc07dcd5bb9cf3c4d04
Adam Markham <adamjmarkham@gmail.com>
2018-10-05 09:13:57 +0100

    Verify get loan returns 200.

--- src/main/java/com/landbay/controller/LoanController.java
+++ src/main/java/com/landbay/controller/LoanController.java
@@ -1,4 +1,18 @@
 package com.landbay.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/loan")
 public class LoanController {
+
+    @GetMapping("/{loanId}")
+    public Object getOffer(@PathVariable(value = "loanId") String loanId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
